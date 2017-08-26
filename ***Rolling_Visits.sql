a9fc1c1e7a717f29c5c1ae6d5f3151f29da5e1f6
smallfishxz <xiaozheyu@gmail.com>
2017-08-26 01:25:52 -0700

    Update ***Rolling_Visits.sql

--- ***Rolling_Visits.sql
+++ ***Rolling_Visits.sql
@@ -4,7 +4,8 @@ Solution 1:
 Create a table with
 
 Id, LastVisit, DaysConsecutivelyVisited
-and just update the table appropriately on each visit. The logic is clear, and there is no need for an ugly SQL query to otherwise extract the desired information.
+and just update the table appropriately on each visit. 
+The logic is clear, and there is no need for an ugly SQL query to otherwise extract the desired information.
 
 Solution 2: 
 uid, login_ts
@@ -16,3 +17,62 @@ Problem 4
 
 Good reference 3:
 https://blog.jooq.org/2015/11/07/how-to-find-the-longest-consecutive-series-of-events-in-sql/
+
+Implementation:
+
+Table log: 
+date          
+----------
+2010-11-26
+2010-11-27
+2010-11-29
+2010-11-30
+2010-12-01
+2010-12-02
+2010-12-03
+2010-12-05
+2010-12-06
+2010-12-07
+2010-12-08
+2010-12-09
+2010-12-13
+2010-12-14
+
+select min(date), max(date), count(*) as consecutive_days
+(select 
+log.*, 
+row_number() over (order by date) as rnk,
+dateadd(day, -row_number() over (order by date), date) as grp
+from log) log
+group by grp
+
+or with CTE(common table expression):
+
+WITH
+  -- This table contains all the distinct date 
+  -- instances in the data set
+  dates(date) AS (
+    SELECT DISTINCT CAST(CreationDate AS DATE)
+    FROM Posts
+    WHERE OwnerUserId = ##UserId##
+  ),
+   
+  -- Generate "groups" of dates by subtracting the
+  -- date's row number (no gaps) from the date itself
+  -- (with potential gaps). Whenever there is a gap,
+  -- there will be a new group
+  groups AS (
+    SELECT
+      ROW_NUMBER() OVER (ORDER BY date) AS rn,
+      dateadd(day, -ROW_NUMBER() OVER (ORDER BY date), date) AS grp,
+      date
+    FROM dates
+  )
+SELECT
+  COUNT(*) AS consecutiveDates,
+  MIN(week) AS minDate,
+  MAX(week) AS maxDate
+FROM groups
+GROUP BY grp
+ORDER BY 1 DESC, 2 DESC
+
