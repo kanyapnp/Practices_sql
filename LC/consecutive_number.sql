cc2271fe922798d96735e0c3786792b1ca06258f
smallfishxz <xiaozheyu@gmail.com>
2017-08-26 00:20:34 -0700

    Update consecutive_number.sql

--- LC/consecutive_number.sql
+++ LC/consecutive_number.sql
@@ -1,1 +1,22 @@
+create table #t (Id int, Name char)
 
+insert into #t values
+(1, 'A'),
+(2, 'A'),
+(3, 'B'),
+(4, 'B'),
+(5, 'B'),
+(6, 'B'),
+(7, 'C'),
+(8, 'B'),
+(9, 'B')
+
+
+select name, count(*) 
+from (select t.*,
+             (row_number() over (order by id) -
+              row_number() over (partition by name order by id)
+             ) as grp
+      from t
+     ) t
+group by grp, name;
